d0eb25291ab7ad3fea3d384b525c5ccd6410fa77
ohowe <ohowe@protonmail.com>
2022-06-28 22:13:48 -0600

    Initial Commit

---
+++ Shared/iOSDSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSDSApp.swift
+//  Shared
+//
+//  Created by Oliver Howe on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct iOSDSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
